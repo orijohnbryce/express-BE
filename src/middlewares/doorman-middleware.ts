740ae5f0473b115a65d822d0516b359f21f9351d
ori_b <ori@ori>
2024-08-05 11:48:49 +0300

    comments

--- src/middlewares/doorman-middleware.ts
+++ src/middlewares/doorman-middleware.ts
@@ -6,7 +6,7 @@ import { StatusCode } from "../models/status-enum";
 // only our FE app will have the key, which should be renew from time to time.
 function doorman(req: Request, res: Response, next: NextFunction) {    
     if (req.header("doormanKey") !== appConfig.doormanKey) {
-        res.status(StatusCode.Unauthorized).send("You are not authorized!");
+        res.status(StatusCode.Unauthorized).send("You are not authorized! (doorman)");
         return;
     }    
     next();  
--- src/utils/auth-utils.ts
+++ src/utils/auth-utils.ts
@@ -3,33 +3,35 @@ import jwt from "jsonwebtoken";
 import { appConfig } from "./config";
 import { UnauthorizedError } from "../models/exceptions";
 
-
-export function createToken(user: UserModel): string {    
-    const userWithoutPassword = { ...user };
-    delete userWithoutPassword.password;
-    const container = { userWithoutPassword }; // token container
-    // const option = { expiresIn: "3h" };
-    const option = { };
-    const token = jwt.sign(container, appConfig.tokenSecreteKey, option);
-    return token;
+export function createToken(user: UserModel): string {
+  const userWithoutPassword = { ...user };
+  delete userWithoutPassword.password;
+  const container = { userWithoutPassword }; // token container
+  // const option = { expiresIn: "3h" };
+  const option = {};
+  const token = jwt.sign(container, appConfig.tokenSecreteKey, option);
+  return token;
 }
 
-export function verifyToken(token: string, adminRequired:boolean=false): UserModel {
-    
-    // throw exception or return UserModel if ok
+export function verifyToken(
+  token: string,
+  adminRequired: boolean = false
+): UserModel {
+  // throw exception or return UserModel if ok
 
-    if (!token) throw new UnauthorizedError();
-    
-    try {        
-        const res = jwt.verify(token, appConfig.tokenSecreteKey) as { userWithoutPassword: UserModel }; 
-        
-        if (adminRequired && !res.userWithoutPassword.isAdmin) {
-            throw new Error("");            
-        }
-        return res.userWithoutPassword;
+  if (!token) throw new UnauthorizedError();
 
-    } catch (error) {
+  try {
+    // verify will throw error if not valid
+    const res = jwt.verify(token, appConfig.tokenSecreteKey) as {
+      userWithoutPassword: UserModel;
+    };
 
-        throw new UnauthorizedError()
-    }    
+    if (adminRequired && !res.userWithoutPassword.isAdmin) {
+      throw new Error("");
+    }
+    return res.userWithoutPassword;
+  } catch (error) {
+    throw new UnauthorizedError();
+  }
 }
